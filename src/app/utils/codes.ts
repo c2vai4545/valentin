f5c9be35c146b1273d1168e5be22e44c03609f24
Carlos Núñez <87947697+c2vai4545@users.noreply.github.com>
2025-03-14 16:29:45 -0300

    Update codes.ts
    Se cambian pistas por geneericas para que quede modo demo

--- src/app/utils/codes.ts
+++ src/app/utils/codes.ts
@@ -4,13 +4,20 @@ export interface CodeRoute {
 }
 
 export const validCodes: CodeRoute[] = [
-    { code: "SER1E5L0VE", route: "/living" },
-    { code: "B4TH2GTHR5", route: "/bano" },
-    { code: "P1ZZA2RAMN", route: "/cocina" },
-    { code: "R00M4L0VE5", route: "/habitacion" },
-    { code: "C0DE4LIFE5", route: "/escritorio" },
-    { code: "ADV3NTUR3X", route: "/auto" },
-    { code: "TAMx100pre", route: "/meta" },
+    // { code: "SER1E5L0VE", route: "/living" },
+    { code: "pista1", route: "/living" },
+    // { code: "B4TH2GTHR5", route: "/bano" },
+    { code: "pista2", route: "/bano" },
+    // { code: "P1ZZA2RAMN", route: "/cocina" },
+    { code: "pista3", route: "/cocina" },
+    // { code: "R00M4L0VE5", route: "/habitacion" },
+    { code: "pista4", route: "/habitacion" },
+    // { code: "C0DE4LIFE5", route: "/escritorio" },
+    { code: "pista5", route: "/escritorio" },
+    // { code: "ADV3NTUR3X", route: "/auto" },
+    { code: "pista6", route: "/auto" },
+    // { code: "TAMx100pre", route: "/meta" },
+    { code: "pista7", route: "/meta" },
   // Agrega más códigos y rutas según necesites
 ];
 
